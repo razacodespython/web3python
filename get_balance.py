764871db1576f6c81f9b56af7256b7a2109380f1
razacodespython <76620622+razacodespython@users.noreply.github.com>
2022-05-15 14:09:22 +0200

    read script

--- get_balance.py
+++ get_balance.py
@@ -9,14 +9,17 @@ mumbai_rpc_url = "https://rpc-mumbai.maticvigil.com"
 # The provider is your connection to a blockchain
 web3 = Web3(Web3.HTTPProvider(mumbai_rpc_url))
 
+#log if we're connected or not
 print(web3.isConnected())
 
+#get the blocknumber
 print(web3.eth.blockNumber)
 
+#get the balance
+#balance is shown in Wei, which is a huge number. Wei is likes 'pennies' in the ethereum world
 wallet_address = "0x55c9bBb71a5CC11c2f0c40362Bb691b33a78B764"
-
 print(web3.eth.getBalance(wallet_address))
 
+#balance here is formatted in ether, 
 balance = web3.eth.getBalance(wallet_address)
-
 print(web3.fromWei(balance,"ether"))
